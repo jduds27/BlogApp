326d2d6d1fea3b1236fd4ae71a8ca783bc5cb5fe
Jason Dudley <jason.dudley27@gmail.com>
2017-11-02 23:24:24 -0600

    added blog index page

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,37 @@
+class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @blogs = Blog.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_blog
+    @blog = Blog.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def blog_params
+    params.require(:blog).permit(:title, :body)
+  end
+end
